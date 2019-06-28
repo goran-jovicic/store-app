cbb5a4ecd9fce0aa2eaeb4876e7007738aad1131
Mona Lisa <gugacns94@gmail.com>
2019-06-28 21:20:32 +0200

    zadatak 5

--- src/components/AppProducts.vue
+++ src/components/AppProducts.vue
@@ -1,12 +1,22 @@
 <template>
-
+	<div>
+		<ul>
+			<li v-for="(product,index) in products" :key="index">
+				{{ `${product.id} ${product.name} ${product.quantity}`}}
+			</li>
+		</ul>
+	</div>
 </template>
 
 <script>
-
+import { productService } from '@/services/ProductService'
 
 export default {
-    
+    data () {
+			return {
+				products : productService.list()
+			}
+		}
 }
 </script>
 
